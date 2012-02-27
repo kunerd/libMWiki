7a3ed283173ce2974c55389ea2c48d8078fbab71
Hendrik Kunert <hendrik.kunert@googlemail.com>
2012-02-27 18:13:33 +0100

    change some licence texts

--- source/restparam.h
+++ source/restparam.h
@@ -1,22 +1,22 @@
 /*
- *  libWiki - A Mediawiki client library.
+ *  libMWiki - A Mediawiki client library.
  *
  *  Copyright (C) 2011, 2012 Hendrik Kunert kunerd@users.sourceforge.net
  *
- *  This file is part of libWiki.
+ *  This file is part of libMWiki.
  *
- *  libWiki is free software: you can redistribute it and/or modify
+ *  libMWiki is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  libWiki is distributed in the hope that it will be useful,
+ *  libMWiki is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with libWiki.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with libMWiki.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef RESTPARAM_H
